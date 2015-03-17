9737b447314f043b633f2660e27473b390b84a6e
Francis Lewis <francislewis@users.noreply.github.com>
2015-03-17 17:34:52 +0000

    Auto cache image

--- js/index.js
+++ js/index.js
@@ -73,3 +73,25 @@ Scroller.prototype = {
 
 var scroller = new Scroller();  
 scroller.init();
+
+
+function preloadImages(array) {
+    if (!preloadImages.list) {
+        preloadImages.list = [];
+    }
+    var list = preloadImages.list;
+    for (var i = 0; i < array.length; i++) {
+        var img = new Image();
+        img.onload = function() {
+            var index = list.indexOf(this);
+            if (index !== -1) {
+                // remove image from the array once it's loaded
+                // for memory consumption reasons
+                list.splice(index, 1);
+            }
+        }
+        list.push(img);
+        img.src = array[i];
+    }
+}
+preloadImages(["assets/background-main.jpg"]);
